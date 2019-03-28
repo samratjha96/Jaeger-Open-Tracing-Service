db2dd5233cb8068e8c56967e333a0c934da09a2b
Samrat Jha <samrat.jha@appian.com>
2019-03-27 21:37:52 -0400

    Two new endpoints

--- tracer.js
+++ tracer.js
@@ -1,4 +1,7 @@
 const initTracer = require("jaeger-client").initTracer;
+const express = require("express");
+const request = require("request");
+var endpoints = express();
 
 const config = {
   serviceName: "nodejs-jaeger-tutorial",
@@ -21,11 +24,26 @@ const options = {
 
 const tracer = initTracer(config, options);
 
-const span = tracer.startSpan("EXAMPLE");
+endpoints.listen(3000, () => {
+  console.log("Listening on port 3000");
+})
 
-span.setTag("hello", "world");
-span.log({ foo: "bar" });
+endpoints.get("/url", (req, res, next) => {
+  const span = tracer.startSpan("ENDPOINT");
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+  span.setTag("GET", "url");
+  span.log({ 'event': "Check URL" });
+  span.finish();
+});
 
-console.log("do stuff...");
-
-span.finish();
+endpoints.get("/google", (req, res, next) => {
+  const span = tracer.startSpan("ENDPOINT");
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body); // Print the HTML for the Google homepage.
+  });
+  span.setTag("GET", "Google");
+  span.log({ 'event': "Displaying Google Homepage" });
+  span.finish();
+});
